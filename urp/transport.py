4c7c36c349ff3fd1856d22f27184584abc7e50d5
Spudbe <amcnally.06@hotmail.co.uk>
2025-07-28 13:51:30 +0100

    Update transport.py

--- urp/transport.py
+++ urp/transport.py
@@ -5,33 +5,27 @@ from urp.message import URPMessage
 class AgentServer:
     def __init__(self, agent, host="localhost", port=8000):
         self.agent = agent
-        self.uri = f"ws://{host}:{port}"
-        self.clients = set()
+        self.host = host
+        self.port = port
+        self.url = f"ws://{host}:{port}"
 
-    async def handler(self, websocket, path):
-        self.clients.add(websocket)
-        async for msg in websocket:
-            # Deserialize incoming message
-            incoming = URPMessage.from_json(msg, payload_cls=... )
-            # Let your agent process it (you’ll define process_message)
-            response = await self.agent.process_message(incoming)
-            if response:
-                # Send back any response message
-                await websocket.send(response.to_json(compact=True))
-        self.clients.remove(websocket)
+    async def handler(self, websocket, _path):
+        async for raw in websocket:
+            incoming = URPMessage.from_json(raw, payload_cls=self.agent.payload_cls)
+            response_msg = await self.agent.process_message(incoming)
+            if response_msg:
+                await websocket.send(response_msg.to_json(compact=True))
 
     def start(self):
-        return websockets.serve(self.handler, *self.uri.replace("ws://","").split(":"))
+        return websockets.serve(self.handler, self.host, self.port)
 
 class AgentClient:
-    def __init__(self, sender_name, target_uri):
+    def __init__(self, sender_name, target_url):
         self.sender = sender_name
-        self.target = target_uri
+        self.target_url = target_url
 
     async def send(self, urp_msg: URPMessage):
-        async with websockets.connect(self.target) as ws:
+        async with websockets.connect(self.target_url) as ws:
             await ws.send(urp_msg.to_json(compact=True))
-            # Optionally wait for a reply
             reply = await ws.recv()
-            return URPMessage.from_json(reply, payload_cls=...)
-
+            return URPMessage.from_json(reply, payload_cls=urp_msg.payload.__class__)
