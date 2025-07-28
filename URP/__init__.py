cf245e9ea185fd6aa498547c2a267791b1d25ec5
Spudbe <amcnally.06@hotmail.co.uk>
2025-07-28 12:57:18 +0100

    Add files via upload

---
+++ URP/__init__.py
@@ -0,0 +1,7 @@
+"""Initialize the urp package.
+
+This file makes the `urp` directory a Python package so that modules like
+`urp.agent`, `urp.core`, `urp.ledger` and `urp.knowledge_base` can be
+imported from scripts outside the package. It does not need to contain
+anything else.
+"""
